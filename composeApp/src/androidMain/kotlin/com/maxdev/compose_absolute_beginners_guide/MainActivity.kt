f0c7d3261d113263225a212211107ccb11bc6719
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-12 17:27:32 -0700

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/maxdev/compose_absolute_beginners_guide/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/maxdev/compose_absolute_beginners_guide/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.maxdev.compose_absolute_beginners_guide
 
 import App
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
